5e597b92e5e92238d98ad81bddab868dc3aa1268
icer-CHN <icer.CHN@outlook.com>
2016-02-26 21:10:44 +0800

    delete stuff

--- uninstall-all-modules.bat
+++ uninstall-all-modules.bat
@@ -1,6 +1,8 @@
-adb uninstall com.icer.transsystembar
-adb uninstall com.icer.scrollerdemo
-adb uninstall com.icer.dialogfragmentdemo
-adb uninstall com.icer.multiitemtypelistviewdemo
 adb uninstall com.icer.contacts
-adb uninstall com.icer.thirdplatformlogin
+adb uninstall com.icer.dialogfragmentdemo
+adb uninstall com.icer.listviewdemo
+adb uninstall com.icer.multiitemtypelistviewdemo
+adb uninstall com.icer.scrollerdemo
+adb uninstall com.icer.textviewlinkdemo
+adb uninstall com.icer.transsystembar
+adb uninstall com.icer.wifihotspot
