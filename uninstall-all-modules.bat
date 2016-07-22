a391c590674fd436eb51e04c24382f6799fa80ab
QCWY <QCWY@QCWY.com>
2016-07-22 10:22:31 +0800

    delete stuff

--- uninstall-all-modules.bat
+++
@@ -1,9 +0,0 @@
-adb uninstall com.icer.contacts
-adb uninstall com.icer.dialogfragmentdemo
-adb uninstall com.icer.listviewdemo
-adb uninstall com.icer.multiitemtypelistviewdemo
-adb uninstall com.icer.scrollerdemo
-adb uninstall com.icer.textviewlinkdemo
-adb uninstall com.icer.transsystembar
-adb uninstall com.icer.wifihotspot
-adb uninstall com.icer.mvpdemo
